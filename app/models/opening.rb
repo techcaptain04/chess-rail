a96a5e53f502e4ab8321249efb03e237efce206b
techcaptain04 <andrii.seliuk@outlook.com>
2023-11-26 11:04:05 +1100

    Find next possible moves from known openings
    Given a PGN of a game in progress, return a list of possible next moves
    based on openings in the database.

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -23,4 +23,39 @@ class Opening < ApplicationRecord
       all
     end
   end
+
+  # Given a PGN string, return a list of next moves from known openings
+  # e.g. Opening.next_opening_moves("1. e4 e5 2. Nc3 Nc6")
+  # => ["Bc4", "Bc5", "d3"]
+  def self.next_opening_moves(pgn)
+    matching_openings = Opening.search_pgn(pgn)
+    next_moves = []
+    matching_openings.each do |opening|
+      next_moves << next_move(opening.pgn, pgn)
+    end
+
+    # Remove nils and return unique values
+    next_moves.compact.uniq
+  end
+
+  private
+
+  # Find the next move after a subset of a PGN
+  def self.next_move(full_pgn, subset_pgn)
+    # Split the full PGN into individual moves
+    full_moves = parse_pgn(full_pgn)
+
+    # Split the subset PGN into individual moves
+    subset_moves = parse_pgn(subset_pgn)
+
+    # Find the next move after the subset
+    next_move_index = subset_moves.length
+    full_moves[next_move_index]
+  end
+
+  # Parse PGN and return an array of moves
+  def self.parse_pgn(pgn)
+    # Remove move numbers and split by spaces
+    pgn.gsub(/\d+\./, '').split
+  end
 end
