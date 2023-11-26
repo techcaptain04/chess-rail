c6eaa4624cebbab95b354be1dad43e6534ce2db4
techcaptain04 <andrii.seliuk@outlook.com>
2023-11-26 18:47:14 +1100

    Increase Stockfish depth to 20

--- app/controllers/api/stockfish_controller.rb
+++ app/controllers/api/stockfish_controller.rb
@@ -2,13 +2,9 @@ class Api::StockfishController < ApplicationController
   def next_move
     fen = params[:fen]
 
-    puts "FEN: #{fen}"
-
-    analysis = Stockfish.analyze fen, { :depth => 12 }
+    analysis = Stockfish.analyze fen, { :depth => 20 }
 
     move = analysis[:bestmove]
-
-    puts "Move: #{move}"
 
     render json: { move: move }
   end
